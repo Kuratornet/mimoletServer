7af0bd5c8c1b71d986205d1a60507f62fcb99235
Kuratornet <kuratornet@gmail.com>
2013-06-08 11:24:38 +0300

    gitignore working?

--- MimoletServer/src/main/java/com/mimolet/server/web/OrderController.java
+++ MimoletServer/src/main/java/com/mimolet/server/web/OrderController.java
@@ -1,10 +1,11 @@
 package com.mimolet.server.web;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -22,70 +23,78 @@ import com.mimolet.server.service.OrderService;
 
 @Controller
 public class OrderController {
-	
+    private Log log = LogFactory.getLog(OrderController.class);
+
 	@Autowired
 	private OrderService orderService;
-	
+
 	@RequestMapping("/index")
-    public String listOrder(Map<String, Object> map) {
+	public String listOrder(Map<String, Object> map) {
 
-        map.put("order", new Order());
-        map.put("orderList", orderService.listOrder());
+		map.put("order", new Order());
+		map.put("orderList", orderService.listOrder());
 
-        return "order";
-    }
-	
+		return "order";
+	}
+
 	@RequestMapping("/")
-    public String home() {
-        return "redirect:/index";
-    }
-	
-	@RequestMapping(value="/jsessionid", method = RequestMethod.GET)
+	public String home() {
+		return "redirect:/index";
+	}
+
+	@RequestMapping(value = "/jsessionid", method = RequestMethod.GET)
 	@ResponseBody
 	public String responseBody(@CookieValue("JSESSIONID") String jsessionid) {
 		return jsessionid;
 	}
-	
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addOrder(@ModelAttribute("order") Order order,
-            BindingResult result) {
 
-        orderService.addOrder(order);
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addOrder(@ModelAttribute("order") Order order,
+			BindingResult result) {
 
-        return "redirect:/index";
-    }
+		orderService.addOrder(order);
 
-    @RequestMapping("/delete/{orderId}")
-    public String deleteOrder(@PathVariable("orderId") Integer orderId) {
+		return "redirect:/index";
+	}
 
-        orderService.removeOrder(orderId);
+	@RequestMapping("/delete/{orderId}")
+	public String deleteOrder(@PathVariable("orderId") Integer orderId) {
 
-        return "redirect:/index";
-    }
-    
-//    @RequestMapping(value = "/uploadfile", method = RequestMethod.POST)
-//    public String upladFile(@RequestParam("byteArray") byte[] fileBytes){
-//    	FileOutputStream fos;
-//		try {
-//			fos = new FileOutputStream("C:\\MyDir\\");
-//    	fos.write(fileBytes);
-//    	fos.close();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//    	
-//    	return null;
-//    }
-    
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String handleFormUpload(@RequestParam("file") MultipartFile file) throws IOException {
-      if (!file.isEmpty()) {
-        final File destination = new File("/");
-        System.out.println(destination.getAbsolutePath());
-        file.transferTo(destination);
-      }
-      return null;
-    }
+		orderService.removeOrder(orderId);
+
+		return "redirect:/index";
+	}
+
+	// @RequestMapping(value = "/uploadfile", method = RequestMethod.POST)
+	// public String upladFile(@RequestParam("byteArray") byte[] fileBytes){
+	// FileOutputStream fos;
+	// try {
+	// fos = new FileOutputStream("C:\\MyDir\\");
+	// fos.write(fileBytes);
+	// fos.close();
+	// } catch (IOException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	// return null;
+	// }
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public boolean handleFormUpload(@RequestParam("file") MultipartFile file) {
+		boolean result = false;
+		try {
+			if (!file.isEmpty()) {
+				final File destination = new File("/");
+				System.out.println(destination.getAbsolutePath());
+				file.transferTo(destination);
+			}
+			result = true;
+		} catch (IOException e) {
+			result = false;
+			log.error(file, e);
+		}
+		return result;
+	}
 
 }
